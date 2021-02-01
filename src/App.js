23947e4773fea2e6e5997f966fed9bebcf8ec3b5
zacharyvunguyen <33263084+zacharyvunguyen@users.noreply.github.com>
2021-02-01 12:07:45 -0600

    Added TodoList

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import TodoFeature from "./features/todo";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Zachary Nguyen
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoFeature/>
+
     </div>
   );
 }
--- src/features/todo/index.jsx
+++ src/features/todo/index.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import TodoList from "./Components/TodoList";
 
 TodoFeature.propTypes = {
     
 };
 
 function TodoFeature(props) {
+    const todoList = [
+        {
+            id: 1,
+            title: 'Eat'
+        },
+        {
+            id: 2,
+            title: 'Sleep'
+        },
+        {
+            id: 3,
+            title: 'Code'
+        }
+    ]
+
     return (
-        <div></div>
+        <div>
+            <h3>Todo List</h3>
+            <TodoList todoList={todoList}/>
+        </div>
     );
 }
 
