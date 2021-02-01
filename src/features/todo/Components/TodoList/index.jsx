8f0f5c6ec0edecd20e882395a71b9462cb3c1dd5
zacharyvunguyen <33263084+zacharyvunguyen@users.noreply.github.com>
2021-02-01 15:07:29 -0600

    Finish Lab: Active Todo

--- src/features/todo/Components/TodoList/index.jsx
+++ src/features/todo/Components/TodoList/index.jsx
@@ -1,20 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './styles.scss'
+import './styles.scss';
+import classnames from 'classnames';
 
 TodoList.propTypes = {
     todoList: PropTypes.array,
+    onTodoClick:PropTypes.func,
 };
 
 TodoList.defaultProps = {
     todoList: [],
+    onTodoClick: null,
 };
 
-function TodoList({todoList}) {
+function TodoList({todoList,onTodoClick}) {
+    const handleTodoClick= (todo,idx) => {
+        if (!onTodoClick) return;
+
+        onTodoClick(todo,idx);
+    }
     return (
         <ul className="todo-list">
-            {todoList.map(todo => (
-                <li key={todo.id}>{todo.title}</li>
+            {todoList.map((todo,idx) => (
+                <li
+                    key={todo.id}
+                    className={classnames({
+                        'todo-item': true,
+                        completed: todo.status === 'completed'
+                    })}
+                    onClick={() => handleTodoClick(todo,idx)}
+                >
+                    {todo.title}
+                </li>
             ))}
         </ul>
     );
