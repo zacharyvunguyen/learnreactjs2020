fb08898a1fec306e61ecfd3ae3b7cfcd965623b9
zacharyvunguyen <33263084+zacharyvunguyen@users.noreply.github.com>
2021-02-01 14:40:07 -0600

    Finish Lab: Active Todo

--- src/features/todo/index.jsx
+++ src/features/todo/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import PropTypes from 'prop-types';
 import TodoList from "./Components/TodoList";
 
@@ -7,25 +7,43 @@ TodoFeature.propTypes = {
 };
 
 function TodoFeature(props) {
-    const todoList = [
+    const initTodoList = [
         {
             id: 1,
-            title: 'Eat'
+            title: 'Eat',
+            status: 'new'
         },
         {
             id: 2,
-            title: 'Sleep'
+            title: 'Sleep',
+            status: 'completed'
         },
         {
             id: 3,
-            title: 'Code'
+            title: 'Code',
+            status: 'new'
         }
     ]
 
+    const [todoList,setTodoList]= useState(initTodoList);
+
+    const handleTodoClick=(todo, idx) =>{
+        // clone current array to the new one
+        const newTodoList = [...todoList];
+        console.log(todo,idx);
+
+        //toggle state
+        newTodoList[idx]={
+            ...newTodoList[idx],
+            status: newTodoList[idx].status==='new'? 'completed':'new',
+        };
+        //update todo list
+        setTodoList(newTodoList);
+    }
     return (
         <div>
             <h3>Todo List</h3>
-            <TodoList todoList={todoList}/>
+            <TodoList todoList={todoList} onTodoClick={handleTodoClick}/>
         </div>
     );
 }
